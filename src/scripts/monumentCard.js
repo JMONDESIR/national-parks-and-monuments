ae8da27821a84f15166dfd2483227f74f0bbc816
JMONDESIR <Joey.M.Dev@gmail.com>
2019-03-25 14:58:57 -0500

    park with edit button functionality

--- src/scripts/monumentCard.js
+++ src/scripts/monumentCard.js
@@ -1,14 +1,23 @@
 const MonumentCard = monument => {
 
-        const monumentArticle = buildElement("article",`monument--${monument.id}`)
+        const monumentArticle = buildElement("article", `monument--${monument.id}`)
+        const buttonHolder = document.createElement("div")
+        buttonHolder.classList = "buttonHolder"
         monumentArticle.classList = "wrapper"
         monumentArticle.appendChild(buildElement("h3", undefined, monument.name))
         monumentArticle.appendChild(buildElement("p", undefined, monument.state))
+        monumentArticle.appendChild(buttonHolder)
 
         const deleteMonumentButton = document.createElement("button")
         deleteMonumentButton.classList = "deleteButton"
         deleteMonumentButton.textContent = "Delete Park"
-        monumentArticle.appendChild(deleteMonumentButton)
+        buttonHolder.appendChild(deleteMonumentButton)
         deleteMonumentButton.addEventListener("click", monumentDelete)
+
+        const editMonumentButton = document.createElement("button")
+        editMonumentButton.classList = "editButton"
+        editMonumentButton.textContent = "Edit"
+        buttonHolder.appendChild(editMonumentButton)
+        editMonumentButton.addEventListener("click", monumentEdit)
         return monumentArticle
-      }
+}
